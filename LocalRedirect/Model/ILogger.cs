e4efe3a908d24d7ab41d48ef9a1e803573620a47
Magnus Anderholm <magnus.anderholm@altran.com>
2014-10-15 09:38:07 +0200

    Added logger framework (using fiddler logger internally)

---
+++ LocalRedirect/Model/ILogger.cs
@@ -0,0 +1,22 @@
+﻿namespace Fiddler.LocalRedirect.Model
+{
+    using System;
+    using System.Runtime.CompilerServices;
+    public interface ILogger
+    {
+        bool IsTraceEnabled { get; }
+        bool IsDebugEnabled { get; }
+        bool IsInfoEnabled { get; }
+        bool IsWarnEnabled { get; }
+        bool IsErrorEnabled { get; }
+        bool IsFatalEnabled { get; }
+
+
+
+        void Debug(Func<string> message, Exception e = null, [CallerMemberName]string callingMethod = "");
+        void Error(Func<string> message, Exception e = null, [CallerMemberName]string callingMethod = "");
+        void Fatal(Func<string> message, Exception e = null, [CallerMemberName]string callingMethod = "");
+        void Info(Func<string> message, Exception e = null, [CallerMemberName]string callingMethod = "");
+        void Warn(Func<string> message, Exception e = null, [CallerMemberName]string callingMethod = "");
+    }
+}
---
+++ LocalRedirect/Model/Logger.cs
@@ -0,0 +1,72 @@
+﻿namespace Fiddler.LocalRedirect.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+
+
+    class Logger : ILogger
+    {
+        private readonly Fiddler.Logger logger;
+        public enum LogLevel { Debug = 0, Info = 1, Warn = 2, Error = 3, Fatal = 4 };
+        private readonly string name;
+        private const string logFormat = "[{0}] ({1}.{2}) - {3}";
+
+        public Logger(string name)
+        {
+            logger = FiddlerApplication.Log;
+            this.name = name;
+        }
+
+        public bool IsTraceEnabled { get { return true; } }
+        public bool IsDebugEnabled { get { return true; } }
+        public bool IsInfoEnabled { get { return true; } }
+        public bool IsWarnEnabled { get { return true; } }
+        public bool IsErrorEnabled { get { return true; } }
+        public bool IsFatalEnabled { get { return true; } }
+
+        public void Debug(Func<string> message, Exception e = null, [CallerMemberName]string callingMethod = "")
+        {
+            Log(LogLevel.Debug, message, e, callingMethod);
+        }
+
+
+        public void Info(Func<string> message, Exception e = null, [CallerMemberName]string callingMethod = "")
+        {
+            Log(LogLevel.Info, message, e, callingMethod);
+        }
+
+
+        public void Warn(Func<string> message, Exception e = null, [CallerMemberName]string callingMethod = "")
+        {
+            Log(LogLevel.Warn, message, e, callingMethod);
+        }
+
+
+        public void Error(Func<string> message, Exception e = null, [CallerMemberName]string callingMethod = "")
+        {
+            Log(LogLevel.Error, message, e, callingMethod);
+        }
+
+
+        public void Fatal(Func<string> message, Exception e = null, [CallerMemberName]string callingMethod = "")
+        {
+            Log(LogLevel.Fatal, message, e, callingMethod);
+        }
+
+        private void Log(LogLevel logLevel, Func<string> message, Exception e, string callingMethod)
+        {
+            string logMsg = string.Format(
+                logFormat,                                
+                logLevel.ToString(),
+                name,
+                callingMethod,
+                message());
+            if (e != null)
+                logMsg += Environment.NewLine + e.ToString();
+            logger.LogString(logMsg);            
+        }
+    }
+}
