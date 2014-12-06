ff484b724c4f68bae52d17bc610c3e955ffe1110
Magnus Anderholm <magnus.anderholm@gmail.com>
2014-12-06 16:42:48 +0100

    Refactored EmbeddedAssemblyLoader so it preloads all embedded assemblies instead of  loading on demand

--- LocalRedirect/Embedded/EmbeddedAssemblyLoader.cs
+++ LocalRedirect/Embedded/EmbeddedAssemblyLoader.cs
@@ -1,42 +1,46 @@
 ﻿namespace Fiddler.LocalRedirect.Embedded
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
 
-    public class EmbeddedAssemblyLoader
+    public class EmbeddedAssemblyLoader : IEnumerable<Assembly>
     {
-        public EmbeddedAssemblyLoader()
-        {
-            // TODO Rewrite so we load all assemblies immediatly and then we only
-            //      look in AppDomain.CurrentDomain.GetAssemblies() and return the correct one
-            //      in OnAssemblyResolve(). Will probably give us a little performance boost if
-            //      we need to load lots of assemblies.
+        private readonly IList<Assembly> embeddedAssemblies = new List<Assembly>();
+        
+        public EmbeddedAssemblyLoader(string @namespace)
+        {            
             AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
-        }                
+            
+            // Load all dll embedded resources that exists in the given @namespace (or subnamespaces)            
+            foreach (var res in GetType().Assembly.GetEmbeddedResources())
+            {
+                if (res.Name.StartsWith(@namespace) && res.Name.EndsWith(".dll", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    using (var s = res.Open())
+                    {
+                        var dllContent = new byte[s.Length];
+                        s.Read(dllContent, 0, dllContent.Length);
+                        embeddedAssemblies.Add(Assembly.Load(dllContent));
+                    }
+                }
+            }
+        }
 
         private Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
         {            
-            Assembly loadedAssembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.FullName == args.Name); ;
-            if (loadedAssembly != null)
-                return loadedAssembly;
-            var assemblyName = args.Name.Split(',')[0] + ".dll";
-            var dll = GetType()
-                .Assembly
-                .GetEmbeddedResources()
-                .FirstOrDefault(e => e.Name.EndsWith(assemblyName, StringComparison.InvariantCultureIgnoreCase));
-            
-            if (dll != null)
-            {
-                using (var s = dll.Open())                
-                {
-                    var dllContent = new byte[s.Length];
-                    s.Read(dllContent, 0, dllContent.Length);
-                    loadedAssembly = Assembly.Load(dllContent);
-                }            
-            }
-                
-            return loadedAssembly;
+            return embeddedAssemblies.FirstOrDefault(a => a.FullName == args.Name);            
+        }
+
+        public IEnumerator<Assembly> GetEnumerator()
+        {
+            return embeddedAssemblies.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
